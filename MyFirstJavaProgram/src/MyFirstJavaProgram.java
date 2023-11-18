b12a52ac12c5e55317059f473b363d6a1866f468
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2023-11-19 00:41:51 +0100

    initial commit

---
+++ MyFirstJavaProgram/src/MyFirstJavaProgram.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class MyFirstJavaProgram {
+
+	public static void main(String[] args) {
+		/*
+		 * 
+		 * System.out.println("Enter your name: ");
+		
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		
+		// IF ELSE IF ELSE
+		if(s.equals("Dabdoube")) {
+			System.out.println("Hello, Dabdoube!");
+		} else if (s.equals("Dabdoub")) {
+			System.out.println("Hi, Dabdoub!");
+		} else {
+			System.out.println("You're not welcome here");
+		}
+		
+		//WHILE
+		Scanner in = new Scanner(System.in);
+		int i = 0;
+		while(i<10) {
+			System.out.println("i is: " + i);
+			i++;
+		}
+		
+		//FOR
+		Scanner in = new Scanner(System.in);
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		
+		//DO WHILE
+		Scanner in = new Scanner(System.in);
+		int i = 0;
+		do{
+			System.out.println("i is: " + i);
+			i++;
+		} while(i < 10); 
+		
+		in.close();
+		*/
+		
+		
+	}
+	
+
+}
