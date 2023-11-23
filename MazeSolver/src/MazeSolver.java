abf5e0396a294959865ea876fc972f97fbc6a54d
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2023-11-23 13:05:15 +0100

    refactored w/ maze object

--- MazeSolver/src/MazeSolver.java
+++ MazeSolver/src/MazeSolver.java
@@ -1,94 +1,176 @@
 import java.util.LinkedList;
 
 public class MazeSolver {
-	static int[][] maze = { //static in order to be accessed in the main without creating an instance
-		{2, 1, 1, 1},
-		{0, 0, 1, 1},
-		{0, 0, 0, 1}
-	};
-	//0: wall	1:path	2:destination
-	
-	static LinkedList <Position> path = new LinkedList<Position>(); //storing the visited path
+	static Maze m = new Maze(); //static in order to be accessed in the main without creating an instance
 	
 	public static void main(String[] args) {
-		Position p = new Position(0, 3); //starting position
-		path.push(p); //using a stack in order to be able to backtrack
-	
-		while(true) {
-			//getting the most recent position
-			int y = path.peek().y;
-			int x = path.peek().x;
+		
+		int[][] maze = { 
+				{2, 1, 1, 1},
+				{0, 0, 1, 1},
+				{0, 0, 0, 1}
+		};
+		m.maze = maze;
+		m.start = new Position(0, 3); //starting position
+		m.path = new LinkedList<Position>();  //storing the visited path
+				
+		if (solveMaze(m.start)) {
+			System.out.println("You won!!");
+		} else {
+			System.out.println("No path...");
+		}
+
+	}
+		
+		private static boolean solveMaze(Position p) {
+			m.path.push(p); //using a stack in order to be able to backtrack
 			
-			//changing each visited spot to a 0
-			maze[y][x]  = 0;
-			
-			//going down
+			while(true) {
+				//getting the most recent position
+				int y = m.path.peek().y;
+				int x = m.path.peek().x;
+				
+				//changing each visited spot to a 0
+				m.maze[y][x]  = 0;
+					
+						
+			//going down	
 			if (isValid(y+1, x)) {
-				if(maze[y+1][x] == 2) { 		
-					System.out.println("Moved down. You won!!");
-					return;
-				} else if(maze[y+1][x] == 1) {
+				if(m.maze[y+1][x] == 2) { 		
+					System.out.println("Moved down.");
+					return true;
+				} else if(m.maze[y+1][x] == 1) {
 					System.out.println("Moved down");
-					path.push(new Position(y+1, x)); //adding a position
+					m.path.push(new Position(y+1, x)); //adding a position
 					continue;
 				}
 			}
 			
 			//going left
 			if (isValid(y, x-1)) {
-				if(maze[y][x-1] == 2) { 		
-					System.out.println("Moved left. You won!!");
-					return;
-				} else if(maze[y][x-1] == 1) {
+				if(m.maze[y][x-1] == 2) { 		
+					System.out.println("Moved left.");
+					return true;
+				} else if(m.maze[y][x-1] == 1) {
 					System.out.println("Moved left");
-					path.push(new Position(y, x-1)); //adding a position
+					m.path.push(new Position(y, x-1)); //adding a position
 					continue;
 				}
 			}
 			
 			//going up
 			if (isValid(y-1, x)) {
-				if(maze[y-1][x] == 2) { 		
-					System.out.println("Moved up. You won!!");
-					return;
-				} else if(maze[y-1][x] == 1) {
+				if(m.maze[y-1][x] == 2) { 		
+					System.out.println("Moved up.");
+					return true;
+				} else if(m.maze[y-1][x] == 1) {
 					System.out.println("Moved up");
-					path.push(new Position(y-1, x)); //adding a position
+					m.path.push(new Position(y-1, x)); //adding a position
 					continue;
 				}
 			}
 			
 			//going right
 			if (isValid(y, x+1)) {
-				if(maze[y][x+1] == 2) { 		
-					System.out.println("Moved right. You won!!");
-					return;
-				} else if(maze[y][x+1] == 1) {
+				if(m.maze[y][x+1] == 2) { 		
+					System.out.println("Moved right.");
+					return true;
+				} else if(m.maze[y][x+1] == 1) {
 					System.out.println("Moved right");
-					path.push(new Position(y, x+1)); //adding a position
+					m.path.push(new Position(y, x+1)); //adding a position
 					continue;
 				}
 			}
-			
-			path.pop();//if unable to move in any direction, backtrack: remove current spot from stack
+				
+			m.path.pop();//if unable to move in any direction, backtrack: remove current spot from stack
 			System.out.println("Moved back!");
-			
-			if(path.size() <= 0) { //
+			if(m.path.size() <= 0) { //
 				System.out.println("No path");
-				return;
+				return false;
 			}
+				
 		}
-
+		
 	}
-	
+
 	public static boolean isValid(int y, int x) { //checking for outer bounds
 		if(y < 0 || 
-		   y >= maze.length ||
+		   y >= m.maze.length ||
 		   x < 0 ||
-		   x >= maze[y].length //for x, need to check the length of the current row
+		   x >= m.maze[y].length //for x, need to check the length of the current row
 		) {
 			return false;
 		}
 		return true;
 	}
+	
 }
+
+//REFACTORING
+
+/*
+private static boolean solveMaze(Position p) {
+	path.push(p); //using a stack in order to be able to backtrack
+	
+	while(true) {
+		//getting the most recent position
+		int y = path.peek().y;
+		int x = path.peek().x;
+		
+		//changing each visited spot to a 0
+		maze[y][x]  = 0;
+		*/
+
+/*
+System.out.println("Moved down.");
+if (move(y+1, x)) {
+	return true;
+} else {
+	System.out.println("Moved left.");
+	if (move(y, x-1)) {
+		return true;
+	} else {
+		System.out.println("Moved up.");	
+		if (move(y-1, x)) {
+			return true;
+		} else {
+			System.out.println("Moved right.");	
+			if (move(y, x+1)) {
+				return true;
+			} else {
+				path.pop();//if unable to move in any direction, backtrack: remove current spot from stack
+				System.out.println("Moved back!");
+				if(path.size() <= 0) { //
+					System.out.println("No path");
+					return false;
+				}
+			}
+		}
+	}
+} 
+}
+}
+	
+private static boolean move(int y, int x) {
+if (isValid(y, x)) {
+	if(maze[y][x] == 2) { 		
+		return true;
+	} else if(maze[y][x] == 1) {
+		path.push(new Position(y, x)); //adding a position
+		return false;
+	}
+}
+return false;
+}
+
+public static boolean isValid(int y, int x) { //checking for outer bounds
+if(y < 0 || 
+   y >= maze.length ||
+   x < 0 ||
+   x >= maze[y].length //for x, need to check the length of the current row
+) {
+	return false;
+}
+return true;
+}
+*/
